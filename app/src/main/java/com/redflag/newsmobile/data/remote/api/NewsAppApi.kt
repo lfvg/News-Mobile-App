ab09ab050f9642564f556b58880a9f63e6f5a1c3
Gilberto Medeiros <62359000+gilbertomoj@users.noreply.github.com>
2025-04-11 01:28:58 -0300

    Update new API key

--- app/src/main/java/com/redflag/newsmobile/data/remote/api/NewsAppApi.kt
+++ app/src/main/java/com/redflag/newsmobile/data/remote/api/NewsAppApi.kt
@@ -10,14 +10,14 @@ import retrofit2.http.Query
 interface NewsAppApi {
 
     @Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
-    @GET("/v2/top-headlines?country=us&apiKey=f887caf7b156483d97fabdd491b92fa5")
+    @GET("/v2/top-headlines?country=us&apiKey=d476f1b66ec3447490f84f2a20abc0ee")
     suspend fun getHeadLines(): Response<NewsResponse>
 
     @Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
     @GET("v2/everything")
-    suspend fun searchNews(@Query("q") queryText: String, @Query("apiKey") apiKey: String = "f887caf7b156483d97fabdd491b92fa5"): Response<NewsResponse>
+    suspend fun searchNews(@Query("q") queryText: String, @Query("apiKey") apiKey: String = "d476f1b66ec3447490f84f2a20abc0ee"): Response<NewsResponse>
 
     @Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
     @GET("v2/everything/sources")
-    suspend fun searchNewsWithFilter(@Query("q") queryText: String, @Query("apiKey") apiKey: String = "f887caf7b156483d97fabdd491b92fa5", @Query("country") country: String? = null, @Query("category") category: String? = null, @Query("language") language: String? = null): Response<NewsResponse>
+    suspend fun searchNewsWithFilter(@Query("q") queryText: String, @Query("apiKey") apiKey: String = "d476f1b66ec3447490f84f2a20abc0ee", @Query("country") country: String? = null, @Query("category") category: String? = null, @Query("language") language: String? = null): Response<NewsResponse>
 }
